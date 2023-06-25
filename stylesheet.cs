20802d41c9b65f09ec22c94ba2efb55f7b6a3bfb
Tobeyo <101884407+Tobeyo@users.noreply.github.com>
2023-06-25 17:28:04 +0200

    upload

---
+++ stylesheet.cs
@@ -0,0 +1,76 @@
+nav{
+    position: fixed;
+    float:left;
+     width: 120px;
+      height: 100%;
+    }
+
+aside{
+    float:right;
+     width: 200px;
+      height: 100%;
+    }
+
+article{
+    margin-bottom: 5vh;
+    padding-left:10px;
+    padding-right: 10px;
+    margin-left:290px ; 
+    margin-right: 290px; 
+    height: auto; 
+    background-color: #3b606e; 
+    opacity: 75%; 
+    line-height: 30px;}
+
+#h{
+    text-align: justify; 
+    font-family:Georgia, 'Times New Roman', Times, serif; font-size: larger;
+}
+
+body{
+    background-image:url(../bilder/background.jpeg); 
+    background-size: cover; 
+    background-attachment: fixed;
+}
+
+h1{
+    padding-top: 20px; 
+}
+
+li{ 
+    top: 50px; 
+    width: 150px; 
+    opacity: 95%;
+    margin-bottom:10px;
+}
+
+#f{
+    position: fixed; 
+    left: 0px; 
+    right: 0px; 
+    bottom: 0px; 
+    top:95vh;
+    text-align: center; 
+    background-color: rgb(84, 84, 84);
+    
+}
+
+p{
+    margin-left: 10px; 
+    margin-right: 10px;
+}
+
+img{
+    opacity: 70%;
+}
+
+a{
+    color:white;
+}
+
+#b{
+     color: rgb(246, 255, 0); 
+     font-size: x-large; background-color: black; 
+     height: 70px;
+	 position: fixed;
+    }
