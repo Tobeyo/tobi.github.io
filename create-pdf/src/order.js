73cdc7845d032b8ca9294062b242e9801e314e10
TobiasNes <101884407+Tobeyo@users.noreply.github.com>
2024-03-15 21:49:13 +0100

    upload

--- create-pdf/src/order.js
+++ create-pdf/src/order.js
@@ -1,18 +1,20 @@
-export function generateOrder() {
+import { faker } from '@faker-js/faker';
+
+export function generateFakeOrder() {
     const items = Array.from(
-        {length: 1},
+        {length: faker.number.int({min: 1, max: 5})},
         () => ({
-            description: "Windows Licence",
-            quantity: "10",
-            cost: "149.99",
+            description: faker.commerce.productName(),
+            quantity: faker.number.int({min: 5, max: 5}),
+            cost: faker.commerce.price(),
         })
     );
-}
 
-return {
-    id: S001,
-    date: new Date(date).toLocaleDateString(),
-    name: "Tobias Nes",
-    items,
-    total: items.reduce((acc, { cost }) => acc + parseFloat(cost), 0),
-};
+    return {
+        id: S001,
+        date: new Date(date).toLocaleDateString(),
+        name: "Tobias Nes",
+        items,
+        total: items.reduce((acc, {cost}) => acc + parseFloat(cost), 0),
+    };
+}
